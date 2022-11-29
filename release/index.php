c0811c560054697e5aa17af9a8ee90f94f0ae980
apparchitekt <73996440+apparchitekt@users.noreply.github.com>
2022-11-29 21:37:25 +0100

    Initial Commit

---
+++ release/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="robots" content="index, follow">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>Choose your streaming service</title>
+
+		<link href="../fonts/roboto-v20-latin/loader.css" rel="stylesheet">
+		<link href="master.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<main>
+			<figure>
+            	<img src="cover.jpg" alt="album cover">
+				<figcaption>Choose your preferred music service:</figcaption>
+			</figure>
+
+			<ul>
+				<li>
+					<a href="https://open.spotify.com/album/2vpwaqdrmOswtyJREHy9ao" title="Listen to the full album on Spotify">
+						<img src="logos/spotify.png" alt="Spotify logo"><em>Play</em>
+					</a>
+				</li>
+				
+				<li>
+					<a href="https://music.apple.com/de/album/scarlet-dawn/1628286633" title="Listen to the full album on Apple Music">
+						<img src="logos/apple.png" alt="Apple Music logo"><em>Play</em>
+					</a>
+				</li>
+				
+				<li>
+					<a href="https://www.youtube.com/watch?v=KDxfdWUJ5fo&list=OLAK5uy_nth8yUFReG6DxxPXS1kJ4aCYeBaDfGsxM" title="Listen to the full album on YouTube Music">
+						<img src="logos/youtube.png" alt="YouTube Music logo"><em>Play</em>
+					</a>
+				</li>
+				
+				<li>
+					<a href="https://music.amazon.de/albums/B0B3F98MJN" title="Listen to the full album on Amazon Music">
+						<img src="logos/amazon.png" alt="Amazon Music logo"><em>Play</em>
+					</a>
+				</li>
+				
+				<li>
+					<a href="https://www.deezer.com/de/album/325464627" title="Listen to the full album on Deezer">
+						<img src="logos/deezer.png" alt="Deezer logo"><em>Play</em>
+					</a>
+				</li>
+				
+				<li>
+					<a href="https://ultradian.bandcamp.com/album/scarlet-dawn" title="Listen to the full album on Bandcamp">
+						<img src="logos/bandcamp.png" alt="Bandcamp logo"><em>Play</em>
+					</a>
+				</li>
+			</ul>
+        </main>
+	</body>
+</html>
